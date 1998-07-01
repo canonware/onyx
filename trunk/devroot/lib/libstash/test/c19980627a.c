c45a603bc68e7e67bdf743760d7e6f1ddf57ec94
jasone <devnull@localhost>
1998-07-01 00:46:08 +0000

    Cleaned up a bit, added a print statement.  This test will still need to be generalized at some point.

--- trunk/devroot/lib/libstash/test/c19980627a.c
+++ trunk/devroot/lib/libstash/test/c19980627a.c
@@ -7,8 +7,8 @@
  *
  * $Source$
  * $Author: jasone $
- * $Revision: 101 $
- * $Date: 1998-06-27 23:38:11 -0700 (Sat, 27 Jun 1998) $
+ * $Revision: 117 $
+ * $Date: 1998-06-30 17:46:08 -0700 (Tue, 30 Jun 1998) $
  *
  * <<< Description >>>
  *
@@ -28,29 +28,32 @@ main()
   
   glob_new();
 
-  _cw_marker("Got here.");
   dbg_clear(g_dbg_o);
-  dbg_turn_on(g_dbg_o, _CW_DBG_C_BRBS_FUNC);
+/*   dbg_turn_on(g_dbg_o, _CW_DBG_C_BRBS_FUNC); */
   dbg_turn_on(g_dbg_o, _CW_DBG_C_BRBS_INIT);
   dbg_turn_on(g_dbg_o, _CW_DBG_C_BRBS_ERROR);
   
-  _cw_marker("Got here.");
   brbs_new(&brbs_o);
   _cw_assert(brbs_is_open(&brbs_o) == FALSE);
 
-  _cw_marker("Got here.");
   _cw_assert(brbs_get_filename(&brbs_o) == NULL);
   _cw_assert(brbs_set_filename(&brbs_o, "/dev/rsd1s1e") == FALSE);
   _cw_assert(brbs_open(&brbs_o) == FALSE);
 
-  _cw_marker("Got here.");
   if (brbs_get_is_raw(&brbs_o))
   {
     log_printf(g_log_o, "Raw device, sector size == %d\n",
 	       brbs_get_sect_size(&brbs_o));
   }
+  {
+    char buf[21];
+    cw_uint64_t size;
+    
+    size = brbs_get_size(&brbs_o);
+    log_print_uint64(size, 16, buf);
+    log_printf(g_log_o, "File size == 0x%s bytes\n", buf);
+  }
   
-  _cw_marker("Got here.");
   brbs_delete(&brbs_o);
   glob_delete();
 
