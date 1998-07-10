c5c29ac2ca0ce918cb2250777a311e0d2aef208e
jasone <devnull@localhost>
1998-07-10 20:45:20 +0000

    Name library 'stash', not 'hungry'.

--- trunk/devroot/lib/c/libstash/src/Makefile.mk
+++ trunk/devroot/lib/c/libstash/src/Makefile.mk
@@ -8,8 +8,8 @@
 #
 # $Source$
 # $Author: jasone $
-# Current revision: $Revision: 131 $
-# Last modified: $Date: 1998-07-07 13:43:45 -0700 (Tue, 07 Jul 1998) $
+# Current revision: $Revision: 138 $
+# Last modified: $Date: 1998-07-10 13:45:20 -0700 (Fri, 10 Jul 1998) $
 #
 ##############################################################################
 
@@ -20,7 +20,7 @@ $(CURRDIR)_LSRCS := bhp.c br.c brblk.c brbs.c buf.c dbg.c depg.c depgn.c \
 	glob.c jt.c list.c locks.c log.c mem.c oh.c res.c sock.c thread.c zt.c
 
 # Mutually exclusive.
-LIB := hungry
+LIB := stash
 
 # Include Makefile magic that uses the above lists.
 include Makefile.common
