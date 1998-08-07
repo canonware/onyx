7647dbb938680cb07e1dd4fb508ee28d402c1d22
jasone <devnull@localhost>
1998-08-07 20:10:54 +0000

    Added buf test.

--- trunk/devroot/lib/libstash/test/Makefile.mk
+++ trunk/devroot/lib/libstash/test/Makefile.mk
@@ -8,8 +8,8 @@
 #
 # $Source$
 # $Author: jasone $
-# Current revision: $Revision: 131 $
-# Last modified: $Date: 1998-07-07 13:43:45 -0700 (Tue, 07 Jul 1998) $
+# Current revision: $Revision: 161 $
+# Last modified: $Date: 1998-08-07 13:10:54 -0700 (Fri, 07 Aug 1998) $
 #
 ##############################################################################
 
@@ -17,7 +17,7 @@
 CURRDIR := test
 
 $(CURRDIR)_CTESTS := c19980411a.c c19980412a.c c19980419a.c c19980430a.c \
-	c19980627a.c c19980630a.c
+	c19980627a.c c19980630a.c c19980731a.c
 $(CURRDIR)_BTESTS :=# List of b*.c back end C code driven by the perl scripts.
 $(CURRDIR)_PTESTS := p19980701a.pl.in
 
