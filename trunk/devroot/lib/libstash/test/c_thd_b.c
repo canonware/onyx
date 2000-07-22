2897d93e600aee871c6c2a86f92a10d68ca6716f
jasone <devnull@localhost>
2000-07-22 00:57:31 +0000

    Use nanosleep() instead of thd_yield() in order to really pause, even if the threads implementation can run threads concurrently.

--- trunk/devroot/lib/libstash/test/c_thd_b.c
+++ trunk/devroot/lib/libstash/test/c_thd_b.c
@@ -13,6 +13,8 @@
 
 #include "../include/libstash/libstash.h"
 
+#include <time.h>
+
 cw_thd_t	thd;
 cw_uint32_t	i;
 
@@ -23,6 +25,11 @@ thread_entry_func(void *a_arg)
 		thd_crit_enter(&thd);
 		i++;
 		thd_crit_leave(&thd);
+
+		/*
+		 * For non-concurrent thread libraries, don't use up the whole
+		 * time quantum, in order to make this test complete faster.
+		 */
 		if (i % 509989 == 0)
 			thd_yield();
 	}
@@ -34,22 +41,23 @@ int
 main()
 {
 	cw_uint32_t	j, count;
-
+	struct timespec	tout = {0, 1000000};
+		
 	libstash_init();
 	_cw_out_put("Test begin\n");
 
 	thd_new(&thd, thread_entry_func, NULL);
-	thd_yield();
+	nanosleep(&tout, NULL);
 
 	_cw_out_put("thd_suspend()\n");
 	for (j = 0; j < 7; j++) {
 		thd_suspend(&thd);
 		count = i;
-		thd_yield();
+		nanosleep(&tout, NULL);
 /*  		_cw_out_put("count([i]) == i([i])\n", count, i); */
 		_cw_assert(count == i);
 		thd_resume(&thd);
-		thd_yield();
+		nanosleep(&tout, NULL);
 		thd_suspend(&thd);
 /*  		_cw_out_put("count([i]) != i([i])\n", count, i); */
 		_cw_assert(count != i);
@@ -60,11 +68,11 @@ main()
 	for (j = 0; j < 7; j++) {
 		while (thd_trysuspend(&thd));
 		count = i;
-		thd_yield();
+		nanosleep(&tout, NULL);
 /*  		_cw_out_put("count([i]) == i([i])\n", count, i); */
 		_cw_assert(count == i);
 		thd_resume(&thd);
-		thd_yield();
+		nanosleep(&tout, NULL);
 		while (thd_trysuspend(&thd));
 /*  		_cw_out_put("count([i]) != i([i])\n", count, i); */
 		_cw_assert(count != i);
