842b0137342ae5f55f541de9d9cca29c88b08044
jasone <devnull@localhost>
2000-07-25 04:26:20 +0000

    Expand various headers to be 80 characters wide.

--- trunk/devroot/lib/c/libstash/include/libstash/dbg.h
+++ trunk/devroot/lib/c/libstash/include/libstash/dbg.h
@@ -1,26 +1,20 @@
-/****************************************************************************
+/******************************************************************************
  *
  * <Copyright = jasone>
  * <License>
  *
- ****************************************************************************
+ ******************************************************************************
  *
  * Version: <Version>
  *
- * <<< Description >>>
- *
  * Public interface for the dbg class.
  *
- ****************************************************************************/
+ ******************************************************************************/
 
 typedef struct cw_dbg_s cw_dbg_t;
 
 cw_dbg_t	*dbg_new(cw_mem_t *a_mem);
-
 void		dbg_delete(cw_dbg_t *a_dbg);
-
 cw_bool_t	dbg_register(cw_dbg_t *a_dbg, const char *a_flag);
-
 void		dbg_unregister(cw_dbg_t *a_dbg, const char *a_flag);
-
 cw_bool_t	dbg_is_registered(cw_dbg_t *a_dbg, const char *a_flag);
