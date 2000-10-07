9ea37c61c601151a8b5a28e5394fba2e368099c8
jasone <devnull@localhost>
2000-10-07 09:19:33 +0000

    Improve command editing support.  History loading/saving and searching are still not implemented, and there are magic values used for initialization.

--- trunk/devroot/bin/stil/src/stil.c
+++ trunk/devroot/bin/stil/src/stil.c
@@ -18,7 +18,15 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define _BUF_SIZE 4096
+#define	_BUF_SIZE	4096
+#define	_PROMPT_STRLEN	  80
+
+/*
+ * Globals.
+ */
+cw_stilt_t	stilt;
+cw_stilts_t	estilts;
+cw_uint8_t	prompt_str[_PROMPT_STRLEN];
 
 char *prompt(EditLine *a_el);
 const char *basename(const char *a_str);
@@ -27,30 +35,12 @@ int
 main(int argc, char **argv)
 {
 	cw_stil_t	stil;
-	cw_stilt_t	stilt;
-	cw_stilts_t	stilts, estilts;
-	char		input[_BUF_SIZE];
-	const char	*str;
-	int		count;
-	ssize_t		bytes_read;
+	cw_stilts_t	stilts;
 	cw_out_t	out;
-	cw_bool_t	is_tty;
-	EditLine	*elp = NULL; /* Shut up the optimizer. */
 
 	libstash_init();
-/*  	dbg_register(cw_g_dbg, "mem_verbose"); */
-/*  	dbg_register(cw_g_dbg, "pezz_verbose"); */
 
 	/* XXX Set up oom handler. */
-
-	is_tty = (cw_bool_t)isatty(0);
-
-	if (is_tty) {
-		elp = el_init(basename(argv[0]), stdin, stdout);
-		el_set(elp, EL_PROMPT, prompt);
-		el_set(elp, EL_EDITOR, "emacs");
-		el_set(elp, EL_SIGNAL, 1);
-	}
 
 	out_new(&out, cw_g_mem);
 	out_set_default_fd(&out, 1);
@@ -72,111 +62,128 @@ main(int argc, char **argv)
 		exit(1);
 	}
 
-#if (0)
-	_cw_out_put("sizeof(cw_stil_t): [i]\n", sizeof(cw_stil_t));
-	_cw_out_put("\n");
-
-	_cw_out_put("sizeof(cw_stilt_t): [i]\n", sizeof(cw_stilt_t));
-	_cw_out_put("sizeof(cw_stilts_t): [i]\n", sizeof(cw_stilts_t));
-	_cw_out_put("\n");
-
-	_cw_out_put("sizeof(cw_stilng_t): [i]\n", sizeof(cw_stilng_t));
-	_cw_out_put("sizeof(cw_stilnt_t): [i]\n", sizeof(cw_stilnt_t));
-	_cw_out_put("\n");
-	
-	_cw_out_put("sizeof(cw_stila_t): [i]\n", sizeof(cw_stila_t));
-	_cw_out_put("sizeof(cw_stilag_t): [i]\n", sizeof(cw_stilag_t));
-	_cw_out_put("sizeof(cw_stilat_t): [i]\n", sizeof(cw_stilat_t));
-	_cw_out_put("\n");
-
-	_cw_out_put("sizeof(cw_stilo_t): [i]\n", sizeof(cw_stilo_t));
-	_cw_out_put("sizeof(cw_stiloe_dicto_t): [i]\n",
-	    sizeof(cw_stiloe_dicto_t));
-	_cw_out_put("\n");
-
-	_cw_out_put("sizeof(cw_stils_t): [i]\n", sizeof(cw_stils_t));
-	_cw_out_put("sizeof(cw_stilso_t): [i]\n", sizeof(cw_stilso_t));
-	_cw_out_put("sizeof(cw_stilsc_t): [i]\n", sizeof(cw_stilsc_t));
-	_cw_out_put("\n");
-
-	_cw_out_put("sizeof(cw_buf_t): [i]\n", sizeof(cw_buf_t));
-	_cw_out_put("sizeof(cw_ch_t): [i]\n", sizeof(cw_ch_t));
-	_cw_out_put("sizeof(cw_chi_t): [i]\n", sizeof(cw_chi_t));
-	_cw_out_put("sizeof(cw_ch_t)[[[i]]: [i]\n", 256,
-	    _CW_CH_TABLE2SIZEOF(256));
-	_cw_out_put("sizeof(cw_ch_t)[[[i]]: [i]\n", 16,
-	    _CW_CH_TABLE2SIZEOF(16));
-	_cw_out_put("sizeof(cw_dch_t): [i]\n", sizeof(cw_dch_t));
-	_cw_out_put("\n");
-#endif
-
-	if (is_tty) {
+	if (isatty(0)) {
 		cw_uint8_t	code[] =
 		    "product print `, version ' print version print \".\n\""
-		    " print\n";
+		    " print flush\n";
+		EditLine	*el;
+		History		*hist;
+		const HistEvent	*hevent;
+		const char	*str;
+		int		count;
 
+		/* Print product and version info. */
 		stilt_interp_str(&stilt, &estilts, code, sizeof(code) - 1);
-	}
 
-	for (;;) {
-#if (0)
-		if (is_tty) {
-			cw_uint8_t	code[] = "prompt\n";
+		/*
+		 * Initialize the command editor.
+		 */
+		hist = history_init();
+		history(hist, H_EVENT, 512);
 
-			stilt_interp_str(&stilt, &estilts, code, sizeof(code) -
-			    1);
-		}
-		/* Read input. */
-		bytes_read = read(0, input, _BUF_SIZE - 1);
-		if (bytes_read <= 0)
-			break;
-		stilt_interp_str(&stilt, &stilts, input,
-		    (cw_uint32_t)bytes_read);
-#else
-		if (is_tty) {
-/*  			const LineInfo	*eline; */
-#if (0)
-			typedef struct lineinfo {
-				const char *buffer;    /* address of buffer */
-				const char *cursor;    /* address of cursor */
-				const char *lastchar;  /*
-							* address of last
-							* character
-							*/
-			} LineInfo;
-#endif
-			if ((str = el_gets(elp, &count)) == NULL) {
-				_cw_out_put_e("Got here\n");
+		el = el_init(basename(argv[0]), stdin, stdout);
+		el_set(el, EL_HIST, history, hist);
+		el_set(el, EL_PROMPT, prompt);
+		el_set(el, EL_EDITOR, "emacs");
+		el_set(el, EL_SIGNAL, 1);
+
+		/* Loop on input until the user quits. */
+		for (;;) {
+			if ((str = el_gets(el, &count)) == NULL)
 				break;
-			}
+			/*
+			 * If not a duplicate, store the command to the
+			 * history.
+			 */
+			hevent = history(hist, H_FIRST);
+			if (hevent == NULL || strcmp(str, hevent->str))
+				history(hist, H_ENTER, str);
 
 			stilt_interp_str(&stilt, &stilts, str,
 			    (cw_uint32_t)count);
-		} else {
-			/* Read input. */
+		}
+
+		/* Clean up the command editor. */
+		el_end(el);
+		history_end(hist);
+	} else {
+		char	input[_BUF_SIZE];
+		ssize_t	bytes_read;
+
+		/* Loop on input until EOF. */
+		for (;;) {
 			bytes_read = read(0, input, _BUF_SIZE - 1);
 			if (bytes_read <= 0)
 				break;
 			stilt_interp_str(&stilt, &stilts, input,
 			    (cw_uint32_t)bytes_read);
 		}
-#endif
-
 	}
 
 	stilts_delete(&estilts, &stilt);
 	stilts_delete(&stilts, &stilt);
 	stilt_delete(&stilt);
 	stil_delete(&stil);
-	if (is_tty)
-		el_end(elp);
 	libstash_shutdown();
 	return 0;
 }
 
-char *prompt(EditLine *a_el)
+char *
+prompt(EditLine *a_el)
 {
-	return "xxx> ";
+	char	*retval;
+
+	if (stilt_deferred(&stilt) == FALSE) {
+		if (stilt_state(&stilt) == STATE_START) {
+			cw_uint8_t	code[] = "prompt\n";
+			cw_uint8_t	*pstr;
+			cw_uint32_t	plen, maxlen;
+			cw_stilo_t	*stilo;
+			cw_stils_t	*stack = stilt_data_stack_get(&stilt);
+
+			/* Push the prompt onto the data stack. */
+			stilt_interp_str(&stilt, &estilts, code, sizeof(code) -
+			    1);
+
+			/* Get the actual prompt string. */
+			stilo = stils_get(stack, 0);
+			pstr = stilo_string_get(stilo);
+			plen = stilo_string_len_get(stilo);
+
+			/* Copy the prompt string to a global buffer. */
+			maxlen = (plen > _PROMPT_STRLEN - 1) ? _PROMPT_STRLEN -
+			    1 : plen;
+			strncpy(prompt_str, pstr, _PROMPT_STRLEN - 1);
+			prompt_str[maxlen] = '\0';
+
+			/* Pop the prompt string off the data stack. */
+			stils_pop(stack, &stilt, 1);
+
+			retval = prompt_str;
+		} else {
+			/*
+			 * Continuation of a string or similarly parsed token.
+			 */
+			retval = "";
+		}
+	} else {
+		/*
+		 * The scanner is in a deferred state right now which means that
+		 * we cannot call the interpreter to get the prompt string.  Use
+		 * the previous value this time around.  This will only result
+		 * in an incorrect prompt if the user types something like:
+		 *
+		 * s> /prompt {"prompt> "} def /foo {
+		 * s> "bar"
+		 * s> } def
+		 * prompt>
+		 *
+		 * Oh well.
+		 */
+		retval = prompt_str;
+	}
+
+	return retval;
 }
 
 /* Doesn't strip trailing '/' characters. */
