51889f2c81e5ff5751dcab53f96569fa0958ea06
jasone <devnull@localhost>
2000-10-07 07:02:33 +0000

    Add initial command prompt editing.

--- trunk/devroot/bin/stil/src/stil.c
+++ trunk/devroot/bin/stil/src/stil.c
@@ -10,6 +10,7 @@
  ******************************************************************************/
 
 #include <libstil/libstil.h>
+#include <libedit/libedit.h>
 #include <libstash/libstash.h>
 
 #include <sys/types.h>
@@ -19,6 +20,9 @@
 
 #define _BUF_SIZE 4096
 
+char *prompt(EditLine *a_el);
+const char *basename(const char *a_str);
+
 int
 main(int argc, char **argv)
 {
@@ -26,9 +30,12 @@ main(int argc, char **argv)
 	cw_stilt_t	stilt;
 	cw_stilts_t	stilts, estilts;
 	char		input[_BUF_SIZE];
+	const char	*str;
+	int		count;
 	ssize_t		bytes_read;
 	cw_out_t	out;
 	cw_bool_t	is_tty;
+	EditLine	*elp = NULL; /* Shut up the optimizer. */
 
 	libstash_init();
 /*  	dbg_register(cw_g_dbg, "mem_verbose"); */
@@ -37,6 +44,13 @@ main(int argc, char **argv)
 	/* XXX Set up oom handler. */
 
 	is_tty = (cw_bool_t)isatty(0);
+
+	if (is_tty) {
+		elp = el_init(basename(argv[0]), stdin, stdout);
+		el_set(elp, EL_PROMPT, prompt);
+		el_set(elp, EL_EDITOR, "emacs");
+		el_set(elp, EL_SIGNAL, 1);
+	}
 
 	out_new(&out, cw_g_mem);
 	out_set_default_fd(&out, 1);
@@ -105,25 +119,85 @@ main(int argc, char **argv)
 	}
 
 	for (;;) {
+#if (0)
 		if (is_tty) {
 			cw_uint8_t	code[] = "prompt\n";
 
 			stilt_interp_str(&stilt, &estilts, code, sizeof(code) -
 			    1);
 		}
-
 		/* Read input. */
 		bytes_read = read(0, input, _BUF_SIZE - 1);
 		if (bytes_read <= 0)
 			break;
 		stilt_interp_str(&stilt, &stilts, input,
 		    (cw_uint32_t)bytes_read);
+#else
+		if (is_tty) {
+/*  			const LineInfo	*eline; */
+#if (0)
+			typedef struct lineinfo {
+				const char *buffer;    /* address of buffer */
+				const char *cursor;    /* address of cursor */
+				const char *lastchar;  /*
+							* address of last
+							* character
+							*/
+			} LineInfo;
+#endif
+			if ((str = el_gets(elp, &count)) == NULL) {
+				_cw_out_put_e("Got here\n");
+				break;
+			}
+
+			stilt_interp_str(&stilt, &stilts, str,
+			    (cw_uint32_t)count);
+		} else {
+			/* Read input. */
+			bytes_read = read(0, input, _BUF_SIZE - 1);
+			if (bytes_read <= 0)
+				break;
+			stilt_interp_str(&stilt, &stilts, input,
+			    (cw_uint32_t)bytes_read);
+		}
+#endif
+
 	}
 
 	stilts_delete(&estilts, &stilt);
 	stilts_delete(&stilts, &stilt);
 	stilt_delete(&stilt);
 	stil_delete(&stil);
+	if (is_tty)
+		el_end(elp);
 	libstash_shutdown();
 	return 0;
+}
+
+char *prompt(EditLine *a_el)
+{
+	return "xxx> ";
+}
+
+/* Doesn't strip trailing '/' characters. */
+const char *
+basename(const char *a_str)
+{
+	const char	*retval = NULL;
+	cw_uint32_t	i;
+
+	_cw_check_ptr(a_str);
+
+	i = strlen(a_str);
+	if (i > 0) {
+		for (i--; i > 0; i--) {
+			if (a_str[i] == '/') {
+				retval = &a_str[i + 1];
+				break;
+			}
+		}
+	}
+	if (retval == NULL)
+		retval = a_str;
+	return retval;
 }
