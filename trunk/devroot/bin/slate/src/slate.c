88d2af181a9f74e6bedd574f2e8466b3d5c58c1d
jasone <devnull@localhost>
2001-12-08 22:51:14 +0000

    Add require and mrequire to slate.  Add tslate so that slate can be run in the source tree.

--- trunk/devroot/bin/slate/src/slate.c
+++ trunk/devroot/bin/slate/src/slate.c
@@ -11,17 +11,26 @@
 
 #include "../include/slate.h"
 
+/* Include generated code. */
+#include "slate_nxcode.c"
+
 void	usage(void);
 
 int
 main(int argc, char **argv, char **envp)
 {
-	int	retval;
+	int		retval;
+	cw_nx_t		nx;
+	cw_nxo_t	thread;
 
 	libonyx_init();
+	nx_new(&nx, NULL, argc, argv, envp);
+	nxo_thread_new(&thread, &nx);
 
+	slate_nxcode(&thread);
 	usage();
 
+	nx_delete(&nx);
 	libonyx_shutdown();
 	return retval;
 }
