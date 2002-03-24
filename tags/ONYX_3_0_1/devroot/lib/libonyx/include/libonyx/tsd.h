9655c9b997bb665987f056b52ab4b5cc0e86f22d
anonymous <devnull@localhost>
2002-03-24 01:34:40 +0000

    This commit was manufactured by cvs2svn to create tag 'ONYX_3_0_1'.

---
+++ tags/ONYX_3_0_1/devroot/lib/libonyx/include/libonyx/tsd.h
@@ -0,0 +1,31 @@
+/* -*- mode: c ; c-file-style: "canonware-c-style" -*-
+ ******************************************************************************
+ *
+ * <Copyright = jasone>
+ * <License>
+ *
+ ******************************************************************************
+ *
+ * Version: Onyx <Version = onyx>
+ *
+ ******************************************************************************/
+
+/* Pseudo-opaque type. */
+typedef struct cw_tsd_s cw_tsd_t;
+
+struct cw_tsd_s
+{
+    pthread_key_t key;
+};
+
+void
+tsd_new(cw_tsd_t *a_tsd, void (*a_func)(void *));
+
+void
+tsd_delete(cw_tsd_t *a_tsd);
+
+void *
+tsd_get(cw_tsd_t *a_tsd);
+
+void
+tsd_set(cw_tsd_t *a_tsd, void *a_val);
