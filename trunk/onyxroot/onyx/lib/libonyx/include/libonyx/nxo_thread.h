4b7423727a390a2894e307e318513fad6fac651c
jasone <devnull@localhost>
2003-02-28 00:39:37 +0000

    Remove dead code, and properly wrap long lines.

--- trunk/onyxroot/onyx/lib/libonyx/include/libonyx/nxo_thread.h
+++ trunk/onyxroot/onyx/lib/libonyx/include/libonyx/nxo_thread.h
@@ -109,24 +109,14 @@ struct cw_nxoe_thread_s
     /* Cached regular expression state used by the match operator.  This needs
      * to be stored here since the cache is per-thread, and there is no other
      * reasonable place to store the information that provides adequate
-     * performance.  Stuffing this in threaddict would impose a huge performance
-     * penalty due to dstack lookup overhead.
+     * performance.  Stuffing this in threaddict would impose a huge
+     * performance penalty due to dstack lookup overhead.
      *
      * The thread object initializes this structure, reports object
-     * references during GC reference iteration, and frees allocated memory.  Otherwise, all operations on
-     * this structure are done by the regex class. */
+     * references during GC reference iteration, and frees allocated memory.
+     * Otherwise, all operations on this structure are done by the regex class.
+     */
     cw_nxo_regex_cache_t regex_cache;
-
-    /* A 10 element array that stores the matching substring and the first 9
-     * capturing subpattern matches. */
-//    cw_nxo_t regex_matches;
-
-    /* A reference to the string that was most recently matched against.  */
-//    cw_nxo_t regex_input;
-
-    /* Offset into regex_input to start the next match at.  This is only used if
-     * the $c or $g flag is set. */
-//    cw_uint32_t regex_cont;
 #endif
 
     /* Tokenizer state.  If a token is broken across two or more input strings,
