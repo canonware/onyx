19671ea07d2fb16cd1c4c7d34f559e78943262f2
jasone <devnull@localhost>
2003-11-09 19:28:34 +0000

    Update bootstrap code.

--- trunk/onyxroot/onyx/bin/onyx/src/interactive_bootstrap.c
+++ trunk/onyxroot/onyx/bin/onyx/src/interactive_bootstrap.c
@@ -16,5 +16,5 @@
 void
 interactive_nxcode(cw_nxo_t *a_thread)
 {
-    cw_onyx_code(a_thread, "{systemdict begin $promptstring {count cvs `onyx:' exch `> ' 3 ncat} bind def end threaddict begin $resume $stop load def end errordict begin $stop {stdin cvx stopped pop} bind def end $modprompt mclass :singleton :load}bind eval");
+    cw_onyx_code(a_thread, "{systemdict begin $promptstring {count cvs `onyx:' exch `> ' 3 ncat} bind def end threaddict begin $resume {currenterror begin $newerror false def $errorname null def !estack dup scount snpop pop !istack dup scount snpop pop !ostack dup scount snpop pop !dstack dup scount snpop pop end stop} bind def end errordict begin $stop {stdin cvx stopped pop} bind def end $modprompt mclass :singleton :load}bind eval");
 }
